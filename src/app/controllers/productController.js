e2398de479c804002e21b36b71e58f48b9430764
ThanhDang3105 <thanhls1235ls12@gmail.com>
2022-02-28 18:26:23 +0700

    update khong gi ca

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -22,67 +22,68 @@ class ProductController {
     }
 
     stored(req, res, next) {
-        TypeProducts.find({
-                type: req.body.type
-            })
-            .then(type => {
-                if (type.length == 0) {
-                    const typeProducts = new TypeProducts({
-                        type: req.body.type,
-                        miniType: [req.body.miniType]
-                    })
-                    typeProducts.save(err => {
-                        if (err) {
-                            res.json(err)
-                        } else {
-                            next()
-                        }
-                    })
-                    const products = new Products(req.body)
-                    products.save(err => {
-                        if (err) {
-                            res.json(err)
-                        } else {
-                            res.redirect('back')
-                        }
-                    })
-                } else if (type[0].miniType.includes(req.body.miniType)) {
-                    const products = new Products(req.body)
-                    products.save(err => {
-                        if (err) {
-                            res.json(err)
-                        } else {
-                            res.redirect('back')
-                        }
-                    })
-                } else {
-                    const products = new Products(req.body)
-                    products.save(err => {
-                        if (err) {
-                            res.json(err)
-                        } else {
-                            next()
-                        }
-                    })
-                    const newMiniType = [...type[0].miniType, req.body.miniType]
-                    TypeProducts.updateOne({
-                        type: req.body.type
-                    }, {
-                        type: req.body.type,
-                        miniType: newMiniType
-                    })
-                    .then(() => {
-                        res.redirect('back')
-                    })
-                    .catch(err => {
-                        res.json(err)
-                    })
+        res.json(req.body)
+        // TypeProducts.find({
+        //         type: req.body.type
+        //     })
+        //     .then(type => {
+        //         if (type.length == 0) {
+        //             const typeProducts = new TypeProducts({
+        //                 type: req.body.type,
+        //                 miniType: [req.body.miniType]
+        //             })
+        //             typeProducts.save(err => {
+        //                 if (err) {
+        //                     res.json(err)
+        //                 } else {
+        //                     next()
+        //                 }
+        //             })
+        //             const products = new Products(req.body)
+        //             products.save(err => {
+        //                 if (err) {
+        //                     res.json(err)
+        //                 } else {
+        //                     res.redirect('back')
+        //                 }
+        //             })
+        //         } else if (type[0].miniType.includes(req.body.miniType)) {
+        //             const products = new Products(req.body)
+        //             products.save(err => {
+        //                 if (err) {
+        //                     res.json(err)
+        //                 } else {
+        //                     res.redirect('back')
+        //                 }
+        //             })
+        //         } else {
+        //             const products = new Products(req.body)
+        //             products.save(err => {
+        //                 if (err) {
+        //                     res.json(err)
+        //                 } else {
+        //                     next()
+        //                 }
+        //             })
+        //             const newMiniType = [...type[0].miniType, req.body.miniType]
+        //             TypeProducts.updateOne({
+        //                 type: req.body.type
+        //             }, {
+        //                 type: req.body.type,
+        //                 miniType: newMiniType
+        //             })
+        //             .then(() => {
+        //                 res.redirect('back')
+        //             })
+        //             .catch(err => {
+        //                 res.json(err)
+        //             })
                     
-                }
-            })
-            .catch(err => {
-                res.json(err)
-            })
+        //         }
+        //     })
+        //     .catch(err => {
+        //         res.json(err)
+        //     })
     }
 
 }
